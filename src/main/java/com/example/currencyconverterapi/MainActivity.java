516cbfd2cf653e85054a74bb38858593538fb1dd
Ahmad Hamoush <hamoush@outlook.com>
2022-03-27 16:56:57 +0300

    Get Updated Rate from first API

--- src/main/java/com/example/currencyconverterapi/MainActivity.java
+++ src/main/java/com/example/currencyconverterapi/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.currencyconverterapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView rate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rate = (TextView) findViewById(R.id.rate);
+
+
+
     }
+
+    public void fetch_rate(View v) {
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://172.20.10.3/currency_api/api.php";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        rate.setText(response.substring(9,14));
+
+                        Log.d(response, "onResponse: ");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(error.getMessage(), "onErrorResponse: ");
+                rate.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+
+
 }
