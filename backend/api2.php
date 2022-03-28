26b4e977a61991e0efb71ad7d1976cfb744d106d
Ahmad Hamoush <hamoush@outlook.com>
2022-03-28 03:41:56 +0300

    API 2 done

--- backend/api2.php
+++ backend/api2.php
@@ -3,16 +3,16 @@
 	header('Content-Type: application/json; charset=utf-8');
 
 	$response = [];
-	$response["status"] = ""; 
+	$response[] = ""; 
 
 	$amount = @$_POST["amount"] ; //getting post data from android app
 	$rate = @$_POST["rate"];
 	$currency = @$_POST["currency"];
 
 	if(@$_POST["amount"]!== NULL AND @$_POST["rate"]!== NULL AND @$_POST["currency"]!== NULL){
-		$server_response["status"] = "Success"; // respond with success if data is posted
+		$server_response[] = "success"; // respond with success if data is posted
 	}else{
-		$server_response["status"] = "Fail"; //fetching data failed
+		$server_response[] = "fail"; //fetching data failed
 	}
 
 	//inserting posted data to database
@@ -21,27 +21,21 @@
 	$query->execute();
 
 	$lastid = mysqli_insert_id($mysqli); //getting last insert id inorder to access last posted object
-	echo "id: ".$lastid."\n"; 
-
+	
 	$query = $mysqli->prepare("SELECT * FROM rates WHERE id = ?");
 
 	$query->bind_param("i", $lastid); //getting last element
 	$query->execute();
 	$array = $query->get_result();
-	$response = [];
+	$to_be_converted_obj = [];
 
 	while($amount = $array->fetch_assoc()){
-    $response[] = $amount;
+    $to_be_converted_obj[] = $amount;
 	}
 
-	$json_response = json_encode($response);
-	echo "To be converted: ".$json_response; //returning object to be converted
+	$to_be_converted_json = json_encode($to_be_converted_obj);
 
-
-	echo "\n";
-
-
-	$myObj = json_decode($json_response, true); //accessing JSON properties
+	$myObj = json_decode($to_be_converted_json, true); //accessing JSON properties
 	$amount_to_convert = $myObj[0]["amount"];
 	$updated_rate = $myObj[0]["rate"];
 	$currency = $myObj[0]["currency"];
@@ -52,7 +46,8 @@
 
 	}
 	else if($currency == "USD"){ //converting to LBP
-		$conversion =number_format((float)($amount_to_convert * $updated_rate), 3, ',', ',');$converted_currency = 'LBP';
+		$conversion =number_format((float)($amount_to_convert * $updated_rate), 3, ',', ',');
+		$converted_currency = 'LBP';
 	}
 	else{
 		echo "currency not available";
@@ -61,15 +56,14 @@
 	$converted_obj['amount'] = $conversion; //inserting new data into new object
 	$converted_obj['rate'] = $updated_rate;
 	$converted_obj['currency'] = $converted_currency;
-	$converted_json = json_encode($converted_obj); 
-	echo "Converted: ".$converted_json; //returning converted rate;
 
 
-	echo "\n";
+	$to_post['recieved'] = $to_be_converted_obj;
+	$to_post['converted'] = $converted_obj;
+	$to_post['status'] = $server_response;
 
-
-	$json_response = json_encode($server_response);
-	echo "Server Responded with: ".$json_response; //server response
+	$to_post_json = json_encode($to_post);
+	echo $to_post_json;
 
 
 ?>
